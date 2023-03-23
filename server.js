011abea595d412c93581bbe16696081da9abe9ff
Michael Mount <mount.photos@gmail.com>
2023-03-22 19:58:22 -0600

    added comments to Express.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+//Required Remote Module
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 
+
+//Express Obj
 const app = express();
 const PORT = process.env.PORT || 3001;
 
